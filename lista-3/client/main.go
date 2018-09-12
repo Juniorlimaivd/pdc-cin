c0118eda105754634012f509adfdfe356cead180
fms6 <fms6@cin.ufpe.br>
2018-09-11 22:19:44 -0300

    Operations working successfully

--- lista-3/client/main.go
+++ lista-3/client/main.go
@@ -6,6 +6,17 @@ import (
 	"net/rpc"
 )
 
+type AccOpArgs struct {
+	AccID  string
+	Amount float32
+}
+
+type TransferArgs struct {
+	PayerID string
+	PayeeID string
+	Amount  float32
+}
+
 const serverAddress = "localhost"
 
 func main() {
@@ -13,13 +24,42 @@ func main() {
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
+
 	// Synchronous call
-	args := 10.10
-	var reply string
-	err = client.Call("Account.Deposit", args, &reply)
+
+	// Get Balance
+	accID := "AC1"
+	var reply float32
+	err = client.Call("AccountsManager.GetBalance", accID, &reply)
 	if err != nil {
 		log.Fatal("account error:", err)
 	}
-	fmt.Printf("Reply: %s\n", reply)
+	fmt.Printf("Balance: %f\n", reply)
+
+	// Deposit
+	accOpArgs := AccOpArgs{"AC1", 32}
+	var depositReply string
+	err = client.Call("AccountsManager.Deposit", accOpArgs, &depositReply)
+	if err != nil {
+		log.Fatal("account error:", err)
+	}
+	fmt.Printf("Deposit reply: %s\n", depositReply)
+
+	// Withdraw
+	var withdrawReply string
+	err = client.Call("AccountsManager.Withdraw", accOpArgs, &withdrawReply)
+	if err != nil {
+		log.Fatal("account error:", err)
+	}
+	fmt.Printf("Deposit reply: %s\n", withdrawReply)
+
+	// Transfer
+	transferArgs := TransferArgs{"AC1", "AC2", 32}
+	var transferReply string
+	err = client.Call("AccountsManager.Transfer", transferArgs, &transferReply)
+	if err != nil {
+		log.Fatal("account error:", err)
+	}
+	fmt.Printf("Deposit reply: %s\n", transferReply)
 
 }
--- lista-3/server/main.go
+++ lista-3/server/main.go
@@ -8,26 +8,86 @@ import (
 	"net/rpc"
 )
 
+var accounts = map[string]*Account{
+	"AC1": &Account{Balance: 1000},
+	"AC2": &Account{Balance: 2000},
+	"AC3": &Account{Balance: 3000},
+	"AC4": &Account{Balance: 4000}}
+
+type AccountsManager struct {
+	Accs map[string]*Account
+}
+
 type Account struct {
-	Id      string
 	Balance float32
 }
 
-func (acc *Account) Deposit(amount float32, reply *string) error {
+type AccOpArgs struct {
+	AccID  string
+	Amount float32
+}
+
+type TransferArgs struct {
+	PayerID string
+	PayeeID string
+	Amount  float32
+}
+
+func (acc *Account) deposit(amount float32) error {
 	acc.Balance += amount
+	return nil
+}
+
+func (acc *Account) withdraw(amount float32) error {
+	acc.Balance -= amount
+	return nil
+}
+
+func (accMngr *AccountsManager) GetBalance(accID string, reply *float32) error {
+	log.Printf("Getting balance from of %s account", accID)
+	acc := accMngr.Accs[accID]
+
+	*reply = acc.Balance
+	return nil
+}
+
+func (accMngr *AccountsManager) Deposit(args AccOpArgs, reply *string) error {
+	log.Printf("Depositing $ %f into %s account", args.Amount, args.AccID)
+	acc := accMngr.Accs[args.AccID]
+
+	acc.deposit(args.Amount)
+
 	*reply = "amount successfully deposited"
 	return nil
 }
 
-func (acc *Account) Withdraw(amount float32, reply *string) error {
-	acc.Balance -= amount
+func (accMngr *AccountsManager) Withdraw(args AccOpArgs, reply *string) error {
+	log.Printf("Withdrawing $ %f on %s account", args.Amount, args.AccID)
+	acc := accMngr.Accs[args.AccID]
+
+	acc.withdraw(args.Amount)
+
 	*reply = "amount successfully withdrawn"
 	return nil
 }
 
+func (accMngr *AccountsManager) Transfer(args *TransferArgs, reply *string) error {
+	log.Printf("Transfering $%.2f from %s to %s...\n", args.Amount, args.PayerID, args.PayeeID)
+
+	payer := accMngr.Accs[args.PayerID]
+	payee := accMngr.Accs[args.PayeeID]
+
+	payer.withdraw(args.Amount)
+	payee.deposit(args.Amount)
+
+	*reply = "successful transfer"
+	return nil
+}
+
 func main() {
-	account := new(Account)
-	rpc.Register(account)
+	accManager := new(AccountsManager)
+	accManager.Accs = accounts
+	rpc.Register(accManager)
 	rpc.HandleHTTP()
 	l, e := net.Listen("tcp", ":1234")
 	if e != nil {
