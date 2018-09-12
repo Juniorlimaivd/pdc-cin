dc92d969827cff80e71d15a515372c2d97f1598e
fms6 <fms6@cin.ufpe.br>
2018-09-11 21:31:21 -0300

    Create basic client and server version

--- lista-3/client/main.go
+++ lista-3/client/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+)
+
+const serverAddress = "localhost"
+
 func main() {
+	client, err := rpc.DialHTTP("tcp", serverAddress+":1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	// Synchronous call
+	args := 10.10
+	var reply string
+	err = client.Call("Account.Deposit", args, &reply)
+	if err != nil {
+		log.Fatal("account error:", err)
+	}
+	fmt.Printf("Reply: %s\n", reply)
 
 }
--- lista-3/server/main.go
+++ lista-3/server/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
+type Account struct {
+	Id      string
+	Balance float32
+}
+
+func (acc *Account) Deposit(amount float32, reply *string) error {
+	acc.Balance += amount
+	*reply = "amount successfully deposited"
+	return nil
+}
+
+func (acc *Account) Withdraw(amount float32, reply *string) error {
+	acc.Balance -= amount
+	*reply = "amount successfully withdrawn"
+	return nil
+}
+
 func main() {
+	account := new(Account)
+	rpc.Register(account)
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
+	fmt.Scanln()
 
 }
